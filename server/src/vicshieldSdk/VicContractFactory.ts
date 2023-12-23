aaa65c5dfe91b57d3cd6c9b1024dbf68ff76630d
dmcong <congduong@descartes.network>
2023-12-23 15:22:55 +0700

    init: vicshield sdk

---
+++ server/src/vicshieldSdk/VicContractFactory.ts
@@ -0,0 +1,559 @@
+import { InterfaceAbi } from 'ethers'
+
+export const VIC_SHIELD_ABI: InterfaceAbi = [
+  {
+    inputs: [],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Approval',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'issuer',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Fee',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'fee',
+        type: 'uint256',
+      },
+    ],
+    name: 'FeeUpdated',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'Transfer',
+    type: 'event',
+  },
+  {
+    inputs: [],
+    name: 'acceptOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+    ],
+    name: 'allowance',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'spender',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'approve',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'burn',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'contracts',
+    outputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        internalType: 'bytes32',
+        name: 'contractHash',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'uint256',
+        name: 'expirationDate',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'contractValue',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address payable',
+        name: 'recipient',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'threshold',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'signaturesCount',
+        type: 'uint256',
+      },
+      {
+        internalType: 'bool',
+        name: 'contractActive',
+        type: 'bool',
+      },
+      {
+        internalType: 'bool',
+        name: 'contractSigned',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'contractsCount',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes32',
+        name: '_contractHash',
+        type: 'bytes32',
+      },
+      {
+        internalType: 'address[]',
+        name: '_signatories',
+        type: 'address[]',
+      },
+      {
+        internalType: 'uint256',
+        name: '_expirationDate',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: '_contractValue',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address payable',
+        name: '_recipient',
+        type: 'address',
+      },
+    ],
+    name: 'createContract',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'decimals',
+    outputs: [
+      {
+        internalType: 'uint8',
+        name: '',
+        type: 'uint8',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'value',
+        type: 'uint256',
+      },
+    ],
+    name: 'estimateFee',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'issuer',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'minFee',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_contractId',
+        type: 'uint256',
+      },
+      {
+        internalType: 'bytes32',
+        name: '_newContractHash',
+        type: 'bytes32',
+      },
+    ],
+    name: 'modifyContract',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'fee',
+        type: 'uint256',
+      },
+    ],
+    name: 'setFee',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '_contractId',
+        type: 'uint256',
+      },
+    ],
+    name: 'signContract',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes4',
+        name: 'interfaceId',
+        type: 'bytes4',
+      },
+    ],
+    name: 'supportsInterface',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'symbol',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'recipient',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'transfer',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'sender',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'recipient',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'transferFrom',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+]
---
+++ server/src/vicshieldSdk/index.ts
@@ -0,0 +1,86 @@
+import {
+  Interface,
+  JsonRpcProvider,
+  Transaction,
+  getBytes,
+  toBeHex,
+} from 'ethers'
+import { VIC_SHIELD_ABI } from './VicContractFactory'
+import * as web3 from 'web3'
+import * as ethUtil from 'ethereumjs-util'
+import { Chain, VictionMainnet, VictionTestnet } from '@desig/supported-chains'
+import { encode } from 'bs58'
+
+const VICTION_RPCS = {
+  mainnet: 'https://rpc.tomochain.com',
+  testnet: 'https://rpc.testnet.tomochain.com',
+}
+
+const CONTRACT_ADDR = '0xe3bc009559CCC80c009a8Ed0E150b8F3ACb8dfb5'
+
+export default class VicShieldSdk {
+  readonly rpc: string
+  readonly chain: Chain
+  constructor(net: 'mainnet' | 'testnet') {
+    this.rpc = VICTION_RPCS[net]
+    if (net === 'mainnet') this.chain = new VictionMainnet()
+    else this.chain = new VictionTestnet()
+  }
+
+  get provider() {
+    return new JsonRpcProvider(this.rpc)
+  }
+
+  async generateCreateContractTx(payload: {
+    owner: string
+    base64Content: string
+    signatories: string[]
+    expirationDate: Date
+    value: string
+    recipient: string
+  }) {
+    const {
+      owner,
+      base64Content,
+      signatories,
+      expirationDate,
+      value,
+      recipient,
+    } = payload
+
+    const itf = Interface.from(VIC_SHIELD_ABI)
+    const message = web3.utils.soliditySha3(base64Content)
+
+    const data = itf.encodeFunctionData('createContract', [
+      ethUtil.toBuffer(message),
+      signatories,
+      expirationDate,
+      value,
+      recipient,
+    ])
+
+    const params = { to: CONTRACT_ADDR, data }
+    const nonce = await this.provider.getTransactionCount(owner)
+    const { gasPrice } = await this.provider.getFeeData()
+    // const gasLimit = await this.provider.estimateGas({
+    //   from: owner,
+    //   ...params,
+    // })
+    // Validate
+    if (!gasPrice) throw new Error('Invalid gas price')
+
+    const tx = Transaction.from({
+      chainId: this.chain.chainId,
+      nonce,
+      gasLimit: 4000000,
+      gasPrice: toBeHex(gasPrice),
+      type: 0,
+      ...params,
+    })
+
+    return {
+      raw: encode(getBytes(tx.unsignedSerialized)),
+      msg: encode(getBytes(tx.unsignedHash)),
+    }
+  }
+}
