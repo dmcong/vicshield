cea6f78d730ea1c355407d0115f055bcac0f4647
PhongNT <phongnt@vitalify.asia>
2023-12-28 00:50:35 +0700

    fix: fix image and content contract board

--- client/src/view/contract-overview/index.tsx
+++ client/src/view/contract-overview/index.tsx
@@ -33,17 +33,21 @@ function ContractOverview() {
                   display: 'flex',
                   justifyContent: 'center',
                   alignItems: 'center',
+                  borderRadius: 34,
                 }}
               >
-                <Image src={contract1} width="50%" preview={false} />
+                <Image src={contract1} width="75%" preview={false} />
               </div>
               <SpaceVertical style={{ padding: 16 }}>
                 <Typography.Title level={5}>
                   Contract Management
                 </Typography.Title>
-                <Typography.Paragraph>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <Typography.Paragraph style={{ height: 150 }}>
+                  Upon opening the VicShield application, users are directed to
+                  the contract management screen. Here, they can see a list of
+                  contracts they have created, including the name, start date,
+                  and end date. By clicking on each contract, they can view
+                  details, edit.
                 </Typography.Paragraph>
 
                 <Button
@@ -67,15 +71,20 @@ function ContractOverview() {
                   display: 'flex',
                   justifyContent: 'center',
                   alignItems: 'center',
+                  borderRadius: 34,
                 }}
               >
-                <Image src={contract2} width="50%" preview={false} />
+                <Image src={contract2} width="75%" preview={false} />
               </div>
               <SpaceVertical style={{ padding: 16 }}>
                 <Typography.Title level={5}>Upload Contract</Typography.Title>
-                <Typography.Paragraph>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <Typography.Paragraph style={{ height: 150 }}>
+                  On the main interface of VicShield, there is an option that
+                  allows users to upload a new contract. When selecting this
+                  option, a dialog box appears, allowing them to choose a
+                  contract file from their computer. After selecting and
+                  confirming, the contract will be uploaded and stored in the
+                  system
                 </Typography.Paragraph>
 
                 <Button
@@ -99,15 +108,19 @@ function ContractOverview() {
                   display: 'flex',
                   justifyContent: 'center',
                   alignItems: 'center',
+                  borderRadius: 34,
                 }}
               >
-                <Image src={contract3} width="50%" preview={false} />
+                <Image src={contract3} width="75%" preview={false} />
               </div>
               <SpaceVertical style={{ padding: 16 }}>
                 <Typography.Title level={5}>Upload Contract</Typography.Title>
-                <Typography.Paragraph>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <Typography.Paragraph style={{ height: 150 }}>
+                  Another feature on the interface is to create a contract based
+                  on available templates. When clicking on this option, users
+                  will see a list of contract templates to choose from. After
+                  selecting a template, they will be guided to fill in the
+                  necessary information in the corresponding fields.
                 </Typography.Paragraph>
 
                 <Button
